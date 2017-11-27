e0043caffbdc033c32e7c897462e06ab1e422a53
y.maeda <y.maeda@otr-jp.com>
2017-11-27 15:48:03 +0900

    run groovy from shell

--- jenkins/parse_test_result.groovy
+++ jenkins/parse_test_result.groovy
@@ -1,16 +1,14 @@
 import groovy.util.XmlParser
 
-def parse(String basePath){
-    def prefix = "${basePath}/build/test-results/integrationTest/TEST-jp.go.jpo.sa1s99.common.format.integration."
-    def files = [
-        new File("${prefix}HtmlToXmlForAmendApiIntegrationTest.xml"),
-        new File("${prefix}HtmlToXmlApiIntegrationTest.xml")
-    ]
-    
-    def list = files.collect{new XmlParser().parse(it)}.testcase.flatten()
-    
-    def groupedByExceptionClass = list.groupBy{ it.failure.@type[0]}
-    groupedByExceptionClass.collect{ key, value -> "${key} : ${value.size()}"}.join("\n")
-}
+def prefix = "build/test-results/integrationTest/TEST-jp.go.jpo.sa1s99.common.format.integration."
+def files = [
+    new File("${prefix}HtmlToXmlForAmendApiIntegrationTest.xml"),
+    new File("${prefix}HtmlToXmlApiIntegrationTest.xml")
+]
 
-return this
+def list = files.collect{new XmlParser().parse(it)}.testcase.flatten()
+
+def groupedByExceptionClass = list.groupBy{ it.failure.@type[0]}
+groupedByExceptionClass.forEach{ key, value -> println("${key} : ${value.size()}")}
+
+
