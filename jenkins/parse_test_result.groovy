43820d7a942e5cfdb47cbed917b2882cc0b8dab2
y.maeda <y.maeda@otr-jp.com>
2017-11-27 15:26:58 +0900

    assign basePath explicitly

--- jenkins/parse_test_result.groovy
+++ jenkins/parse_test_result.groovy
@@ -1,13 +1,16 @@
 import groovy.util.XmlParser
 
-def basePath = "build/test-results/integrationTest/TEST-jp.go.jpo.sa1s99.common.format.integration."
-def files = [
-    //new File("${basePath}HtmlToXmlForAmendApiIntegrationTest.xml"),
-    new File("${basePath}HtmlToXmlApiIntegrationTest.xml")
-]
+def parse(String basePath){
+    def prefix = "${basePath}/build/test-results/integrationTest/TEST-jp.go.jpo.sa1s99.common.format.integration."
+    def files = [
+        new File("${prefix}HtmlToXmlForAmendApiIntegrationTest.xml"),
+        new File("${prefix}HtmlToXmlApiIntegrationTest.xml")
+    ]
+    
+    def list = files.collect{new XmlParser().parse(it)}.testcase.flatten()
+    
+    def groupedByExceptionClass = list.groupBy{ it.failure.@type[0]}
+    groupedByExceptionClass.collect{ key, value -> "${key} : ${value.size()}"}.join("\n")
+}
 
-def list = files.collect{new XmlParser().parse(it)}.testcase.flatten()
-
-def groupedByExceptionClass = list.groupBy{ it.failure.@type[0]}
-
-return groupedByExceptionClass.collect{ key, value -> "${key} : ${value.size()}"}.join("\n")
+return this
